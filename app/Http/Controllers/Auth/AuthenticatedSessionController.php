ac44cb7e4b684168d9ee1f349ff5548b17c519d0
Otabek <otabekrahmonovtest@gmail.com>
2024-07-16 23:18:12 +0500

    feat: User dashboard template  and logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,7 +34,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->intended('/vendor/dashboard');
         }
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('user.dashboard', absolute: false));
     }
 
     /**
