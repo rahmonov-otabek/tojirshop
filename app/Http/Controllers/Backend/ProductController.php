60df23e48b00c20fa34bf155ab4a92b2c99b3c41
Otabek <otabekrahmonovtest@gmail.com>
2024-07-23 18:06:22 +0500

    feat: Product Update

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -101,7 +101,12 @@ class ProductController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $product = Product::findOrFail($id); 
+        $subCategories = SubCategory::where('category_id', $product->category_id)->get(); 
+        $childCategories = ChildCategory::where('sub_category_id', $product->sub_category_id)->get(); 
+        $categories = Category::all();
+        $brands = Brand::all();
+        return view('admin.product.edit', compact('categories', 'brands', 'product', 'subCategories', 'childCategories'));
     }
 
     /**
@@ -109,7 +114,52 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'image' => ['nullable', 'image', 'max:2048'],
+            'name' => ['string', 'max:200'],
+            'category' => ['required'],
+            'brand' => ['required'],
+            'price' => ['required'],
+            'qty' => ['required'],
+            'short_description' => ['required', 'max:600'],
+            'long_description' => ['required'],
+            'product_type' => ['required'], 
+            'status' => ['required'],
+            'seo_title' => ['nullable', 'max:200'],
+            'seo_description' => ['nullable', 'max:250'],
+        ]);
+
+        $product = Product::findOrFail($id);
+                                
+        /** Handle update file */ 
+        $imagePath = $this->updateImage($request, 'image', 'uploads', $product->thumb_image); 
+
+        $product->thumb_image = empty(!$imagePath) ? $imagePath : $product->thumb_image;
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name); 
+        $product->vendor_id = Auth::user()->vendor->id;;
+        $product->category_id = $request->category;
+        $product->sub_category_id = $request->sub_category;
+        $product->child_category_id = $request->child_category;
+        $product->brand_id = $request->brand;
+        $product->qty = $request->qty;
+        $product->short_description = $request->short_description;
+        $product->long_description = $request->long_description;
+        $product->video_link = $request->video_link;
+        $product->sku = $request->sku;
+        $product->price = $request->price;
+        $product->offer_price = $request->offer_price;
+        $product->offer_start_date = $request->offer_start_date;
+        $product->offer_end_date = $request->offer_end_date;
+        $product->product_type = $request->product_type; 
+        $product->status = $request->status;
+        $product->is_approved = 1;
+        $product->seo_title = $request->seo_title;
+        $product->seo_description = $request->seo_description; 
+        $product->save();
+
+        toastr('Updated Successfully!', 'success');
+        return redirect()->route('admin.product.index');
     }
 
     /**
--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <section class="section">
     <div class="section-header">
-      <h1>Slider</h1> 
+      <h1>Update</h1> 
     </div>
 
     <div class="section-body"> 
@@ -11,46 +11,137 @@
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h4>Edit Slider</h4> 
+              <h4>Update Product</h4> 
             </div>
             <div class="card-body"> 
-                <form action="{{ route('admin.slider.update', $slider->id )}}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('admin.product.update', $product->id )}}" method="POST" enctype="multipart/form-data">
                   @csrf
                   @method("PUT")
+                  <div class="form-group">
+                    <label>Preview</label>
+                    <br>
+                    <img width="200" src="{{ asset($product->thumb_image) }}" alt="">
+                </div>
                     <div class="form-group">
-                        <label>Preview</label>
-                        <br>
-                        <img width="200" src="{{ asset($slider->banner) }}" alt="">
+                        <label>Image</label>
+                        <input type="file" class="form-control" name="image">
                     </div>
                     <div class="form-group">
-                        <label>Banner</label>
-                        <input type="file" class="form-control" name="banner">
+                        <label>Name</label>
+                        <input type="text" class="form-control" name="name" value="{{ $product->name }}">
+                    </div> 
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="inputState">Category</label>
+                          <select id="inputState" class="form-group form-control main-category" name="category">
+                            <option value="">Select</option> 
+                            @foreach ($categories as $category)
+                              <option {{ $category->id == $product->category_id ? 'selected' : '' }}  value="{{ $category->id }}">{{ $category->name }}</option> 
+                            @endforeach
+                          </select> 
+                      </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="inputState">Sub Category</label>
+                          <select id="inputState" class="form-group form-control sub-category" name="sub_category">
+                            <option value="">Select</option> 
+                            @foreach ($subCategories as $subCategory)
+                              <option {{ $subCategory->id == $product->sub_category_id ? 'selected' : '' }}  value="{{ $subCategory->id }}">{{ $subCategory->name }}</option> 
+                            @endforeach
+                          </select> 
+                      </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="inputState">Child Category</label>
+                          <select id="inputState" class="form-group form-control child-category" name="child_category">
+                            <option value="">Select</option> 
+                            @foreach ($childCategories as $childCategory)
+                              <option {{ $childCategory->id == $product->child_category_id ? 'selected' : '' }}  value="{{ $childCategory->id }}">{{ $childCategory->name }}</option> 
+                            @endforeach
+                          </select> 
+                      </div>
+                      </div>
                     </div>
                     <div class="form-group">
-                        <label>Type</label>
-                        <input type="text" class="form-control" name="type" value="{{ $slider->type }}">
+                      <label for="inputState">Brand</label>
+                      <select id="inputState" class="form-group form-control" name="brand">
+                        <option value="">Select</option> 
+                        @foreach ($brands as $brand)
+                          <option {{ $brand->id == $product->brand_id ? 'selected' : '' }} value="{{ $brand->id }}">{{ $brand->name }}</option> 
+                        @endforeach
+                      </select> 
+                  </div>
+                    <div class="form-group">
+                      <label>SKU</label>
+                      <input type="text" class="form-control" name="sku" value="{{ $product->sku }}">
+                    </div> 
+                    <div class="form-group">
+                      <label>Price</label>
+                      <input type="text" class="form-control" name="price" value="{{ $product->price}}">
+                    </div> 
+                    <div class="form-group">
+                      <label>Offer price</label>
+                      <input type="text" class="form-control" name="offer_price" value="{{ $product->offer_price}}">
+                    </div> 
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label>Offer start date</label>
+                          <input type="text" class="form-control datepicker" name="offer_start_date" value="{{ $product->offer_start_date }}">
+                        </div> 
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label>Offer end date</label>
+                          <input type="text" class="form-control datepicker" name="offer_end_date" value="{{ $product->offer_end_date }}">
+                        </div> 
+                      </div>
                     </div>
                     <div class="form-group">
-                        <label>Title</label>
-                        <input type="text" class="form-control" name="title" value="{{ $slider->title }}">
+                      <label>Stock Quantity</label>
+                      <input type="number" min="0" class="form-control" name="qty" value="{{ $product->qty }}">
+                    </div> 
+                    <div class="form-group">
+                      <label>Video link</label>
+                      <input type="text" class="form-control" name="video_link" value="{{ $product->video_link }}">
                     </div>
                     <div class="form-group">
-                        <label>Starting Price</label>
-                        <input type="text" class="form-control" name="starting_price" value="{{ $slider->starting_price }}">
-                    </div>
+                      <label>Short Description</label>
+                      <textarea name="short_description" class="form-control">{!! $product->short_description !!}</textarea> 
+                    </div> 
                     <div class="form-group">
-                        <label>Button url</label>
-                        <input type="text" class="form-control" name="btn_url" value="{{ $slider->btn_url }}">
+                      <label>Long Description</label>
+                      <textarea name="long_description" class="form-control summernote">{!! $product->long_description !!}</textarea> 
                     </div>
+
+                   
+                        <div class="form-group">
+                          <label for="inputState">Product Type</label>
+                          <select id="inputState" class="form-group form-control" name="product_type">
+                              <option value="0">Select</option>
+                              <option {{ $product->product_type == 'new_arrival' ? 'selected' : '' }} value="new_arrival">New Arrival</option>
+                              <option {{ $product->product_type == 'featured_product' ? 'selected' : '' }} value="featured_product">Featured</option>
+                              <option {{ $product->product_type == 'top_product' ? 'selected' : '' }} value="top_product">Top Product</option>
+                              <option {{ $product->product_type == 'best_product' ? 'selected' : '' }} value="best_product">Best Product</option> 
+                          </select> 
+                      </div>                
+                  
                     <div class="form-group">
-                        <label>Serial</label>
-                        <input type="text" class="form-control" name="serial" value="{{ $slider->serial }}">
-                    </div>
+                      <label>Seo Title</label>
+                      <input type="text" class="form-control" name="seo_title" value="{{ $product->seo_title }}">
+                  </div>
+                  <div class="form-group">
+                    <label>Seo Description</label>
+                    <textarea name="seo_description" class="form-control">{!! $product->seo_description !!}</textarea> 
+                </div>
                     <div class="form-group">
                         <label for="inputState">Status</label>
-                        <select id="inputState" class="form-group" name="status">
-                            <option {{ $slider->status == 1 ? 'selected' : '' }} value="1">Active</option>
-                            <option {{ $slider->status == 0 ? 'selected' : '' }} value="0">Inactive</option>
+                        <select id="inputState" class="form-group form-control" name="status">
+                            <option {{ $product->status == 1 ? 'selected' : '' }} value="1">Active</option>
+                            <option {{ $product->status == 0 ? 'selected' : '' }} value="0">Inactive</option>
                         </select> 
                     </div>
                     <button type="submit" class="btn btn-primary">Update</button>
@@ -62,3 +153,52 @@
     </div>
   </section>
 @endsection
+
+@push('scripts')
+  <script>
+    $(document).ready(function(){
+      $('body').on('change', '.main-category', function(e){
+        $('.child-category').html('<option value="">Select</option>');
+
+        let id = $(this).val();
+        $.ajax({
+          method: 'GET',
+          url: "{{ route('admin.product.get-subcategories') }}",
+          data: {
+            id: id 
+          },
+          success: function(data){
+            $('.sub-category').html('<option value="">Select</option>');
+
+            $.each(data, function(i, item){
+              $('.sub-category').append(`<option value="${item.id}">${item.name}</option>`); 
+            });
+          },
+          error: function(xhr, status, error){
+            console.log(error); 
+          }
+        });
+      });
+      $('body').on('change', '.sub-category', function(e){
+        let id = $(this).val();
+        $.ajax({
+          method: 'GET',
+          url: "{{ route('admin.product.get-childcategories') }}",
+          data: {
+            id: id 
+          },
+          success: function(data){
+            $('.child-category').html('<option value="">Select</option>');
+
+            $.each(data, function(i, item){
+              $('.child-category').append(`<option value="${item.id}">${item.name}</option>`); 
+            });
+          },
+          error: function(xhr, status, error){
+            console.log(error); 
+          }
+        });
+      });
+    });
+  </script>
+@endpush
