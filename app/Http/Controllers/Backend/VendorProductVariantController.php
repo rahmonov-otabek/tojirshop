03b2392ead22b16c5e7159c20264cd16c27061de
Otabek <otabekrahmonovtest@gmail.com>
2024-08-06 11:37:33 +0500

    fix

--- app/Http/Controllers/Backend/VendorProductVariantController.php
+++ app/Http/Controllers/Backend/VendorProductVariantController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\ProductVariant;
 use App\Models\Product;
 use App\Models\ProductVariantItem;
+use Illuminate\Support\Facades\Auth;
 
 class VendorProductVariantController extends Controller
 {
@@ -16,7 +17,12 @@ class VendorProductVariantController extends Controller
      */
     public function index(VendorProductVariantDataTable $dataTable, Request $request)
     {   
-        $product = Product::findOrFail($request->product); 
+        $product = Product::findOrFail($request->product);
+        
+        if($product->vendor_id != Auth::user()->vendor->id){
+            abort(404);
+        }
+
         return $dataTable->render('vendor.product.product-variant.index',  compact('product'));
     } 
 
@@ -63,7 +69,12 @@ class VendorProductVariantController extends Controller
      */
     public function edit(string $id)
     {
-        $variant = ProductVariant::findOrFail($id);  
+        $variant = ProductVariant::findOrFail($id); 
+        
+        if($variant->product->vendor_id != Auth::user()->vendor->id){
+            abort(404);
+        }
+
         return view('vendor.product.product-variant.edit', compact('variant'));
    
     }
@@ -80,6 +91,10 @@ class VendorProductVariantController extends Controller
 
         $variant = ProductVariant::findOrFail($id);   
   
+        if($variant->product->vendor_id != Auth::user()->vendor->id){
+            abort(404);
+        }
+
         $variant->name = $request->name;  
         $variant->status = $request->status;
         $variant->save();
@@ -95,6 +110,10 @@ class VendorProductVariantController extends Controller
     {
         $variant = ProductVariant::findOrFail($id); 
 
+        if($variant->product->vendor_id != Auth::user()->vendor->id){
+            abort(404);
+        }
+        
         $variantItems = ProductVariantItem::where('product_variant_id', $variant->id)->count();
         if($variantItems>0){
             return response(['status' => 'error', 'message' => 'This items contain variant items in it delete the variant items 
