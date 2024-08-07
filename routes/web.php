c40009f1724f808a8cefa6dd141a3fe6fbdc71cc
Otabek <otabekrahmonovtest@gmail.com>
2024-08-07 23:36:13 +0500

    feat: User Addresses

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Frontend\FlashSaleController;
 use App\Http\Controllers\Frontend\FrontendProductController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserAddressController;
 use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\Frontend\UserProfileController;
 use App\Http\Controllers\ProfileController;
@@ -33,4 +34,7 @@ Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 
     Route::get('profile', [UserProfileController::class, 'index'])->name('profile'); # user.profile
     Route::put('profile', [UserProfileController::class, 'updateProfile'])->name('profile.update'); 
     Route::post('profile', [UserProfileController::class, 'updatePassword'])->name('profile.update.password'); 
+
+    /** User address routes */
+    Route::resource('address', UserAddressController::class);
 });
